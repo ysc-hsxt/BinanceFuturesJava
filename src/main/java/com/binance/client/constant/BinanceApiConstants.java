ebe43ed7afa803f815ca0f115612786b1fc0864d
yonathan95 <yonathan.wolloch@gmail.com>
2021-03-27 18:03:41 +0300

    fixed Market Data Endpoints

--- src/main/java/com/binance/client/constant/BinanceApiConstants.java
+++ src/main/java/com/binance/client/constant/BinanceApiConstants.java
@@ -37,7 +37,7 @@ public class BinanceApiConstants {
     /**
      * Default receiving window.
      */
-    public static final long DEFAULT_RECEIVING_WINDOW = 60_000L;
+    public static final long DEFAULT_RECEIVING_WINDOW = 80_000L;
 
     /**
      * Default ToStringStyle used by toString methods. Override this to change the
